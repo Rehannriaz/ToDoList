429bb4ed1742b5122deada442f98761a2d9f73a0
rehannriaz <u2021438@giki.edu.pk>
2023-08-09 10:18:41 +0500

    posts being added, user can see only his posts

--- javascript/auth.js
+++ javascript/auth.js
@@ -3,27 +3,28 @@ class Auth {
     document.querySelector("body").style.display = "none";
     const auth = localStorage.getItem("auth");
     this.validateAuth(auth);
-}
+  }
 
-validateAuth(auth) {
+  validateAuth(auth) {
     if (auth != 1) {
-        window.location.replace("/pages/login.html");
+      window.location.replace("/pages/login.html");
     } else {
-        document.querySelector("body").style.display = "block";
-        let loginPage = document.getElementById("login-page");
-        let signupPage = document.getElementById("signup-page");
+      document.querySelector("body").style.display = "block";
+      let loginPage = document.getElementById("login-page");
+      let signupPage = document.getElementById("signup-page");
       loginPage.style.display = "none";
       signupPage.style.display = "none";
     }
-}
+  }
 
   logOut() {
     let loginPage = document.getElementById("login-page");
     let signupPage = document.getElementById("signup-page");
-  loginPage.style.display = "block";
-  signupPage.style.display = "block";
+    loginPage.style.display = "block";
+    signupPage.style.display = "block";
     localStorage.removeItem("auth");
-    localStorage.removeItem("userIsLoggedIn")
+    // localStorage.removeItem("todoVal");
+    localStorage.removeItem("userIsLoggedIn");
     window.location.replace("/");
   }
 }
@@ -32,3 +33,38 @@ const auth = new Auth();
 document.querySelector(".logout").addEventListener("click", (e) => {
   auth.logOut();
 });
+
+const todoForm = document.getElementById("todo-form");
+const todo = document.getElementById("todo");
+
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const todoValue = todo.value;
+
+  let todoVal = JSON.parse(localStorage.getItem("todoVal")) || [];
+  const currentUser = localStorage.getItem("userIsLoggedIn");
+  const currentUsername = JSON.parse(currentUser).username;
+  console.log("Current user = " + currentUsername);
+
+  todoVal.push({ post: todoValue, username: currentUsername });
+  localStorage.setItem("todoVal", JSON.stringify(todoVal));
+  todoForm.submit();
+});
+
+const todoList = document.getElementById("todo-list");
+const allTodo = JSON.parse(localStorage.getItem("todoVal"));
+let currentUserPost = [];
+
+if (allTodo) {
+  const currentUsername = JSON.parse(
+    localStorage.getItem("userIsLoggedIn")
+  ).username;
+
+  currentUserPost = allTodo.filter((post) => post.username === currentUsername);
+  const postHTML = currentUserPost
+    .map((post) => `<li>${post.post}</li>`)
+    .join("");
+  todoList.innerHTML = postHTML;
+} else {
+  console.error("user not logged in");
+}
--- javascript/login.js
+++ javascript/login.js
@@ -1,31 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const loginForm = document.getElementById('login-form');
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('password');
-    const errorMessage= document.getElementById('error-message');
+const loginForm = document.getElementById("login-form");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const errorMessage = document.getElementById("error-message");
 
-    loginForm.addEventListener("submit", function(event) {
-        event.preventDefault(); 
-        const email = emailInput.value;
-        const password = passwordInput.value;
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const email = emailInput.value;
+  const password = passwordInput.value;
 
-        let userData = JSON.parse(localStorage.getItem('userData')) || [];
-        let userIsLoggedIn = JSON.parse(localStorage.getItem('userIsLoggedIn')) || [];
-        
-        userIsLoggedIn.push({ email: email, password: password});
-        localStorage.setItem("userIsLoggedIn", JSON.stringify(userIsLoggedIn));
+  let userData = JSON.parse(localStorage.getItem("userData")) || [];
+  //   let userIsLoggedIn = JSON.parse(localStorage.getItem("userIsLoggedIn")) || [];
 
-        // Find matching entry
-        const matchingEntry = userData.find(entry => entry.email === email && entry.password === password);
-        if (matchingEntry) {
-            console.log('Login successful:', matchingEntry);
-            errorMessage.innerText='Login Successful';
-            localStorage.setItem("auth", 1);
-            window.location.href = '/pages/index.html';
- 
-        } else {
-            console.log('Email or password is incorrect');
-            errorMessage.innerText='Email or password is incorrect';
-        }
-    });
+  // Find matching entry
+  const matchingEntry = userData.find(
+    (entry) => entry.email === email && entry.password === password
+  );
+  if (matchingEntry) {
+    let userIsLoggedIn = { username: matchingEntry.username};
+    localStorage.setItem("userIsLoggedIn", JSON.stringify(userIsLoggedIn));
+    console.log("Login successful:", matchingEntry);
+    errorMessage.innerText = "Login Successful";
+    localStorage.setItem("auth", 1);
+    window.location.href = "/pages/index.html";
+  } else {
+    console.log("Email or password is incorrect");
+    errorMessage.innerText = "Email or password is incorrect";
+  }
 });
