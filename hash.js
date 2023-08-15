58f9e6e5aa8f4170a46d1a93bcb76804e43ac142
rehannriaz <u2021438@giki.edu.pk>
2023-08-15 10:39:03 +0500

    .

--- hash.js
+++ hash.js
@@ -1,9 +1,17 @@
-function hashFunc(input) {
-  let hash = "";
-  for (let i = 0; i < input.length; i++) {
-    let tempVal = "";
-    tempVal = input.charCodeAt(i);
-    hash += tempVal.toString(16);
-  }
-  return hash;
+// function hashFunc(input) {
+//   let hash = "";
+//   for (let i = 0; i < input.length; i++) {
+//     let tempVal = "";
+//     tempVal = input.charCodeAt(i);
+//     hash += tempVal.toString(16);
+//   }
+//   return hash;
+// }
+
+// const Hashes = require("jshashes");
+
+function hashFunc(input){
+  var MD5 = new Hashes.MD5().hex(input)
+  return MD5;
 }
+
